8059fd72669cfc4e996189543f3105e438f8beae
Evan Larose <Evan Larose>
2017-07-18 11:28:29 -0300

    Changed some 8-ball replies, factored a line into something better, changed _ing's functonality a bit

--- simplebot.js
+++ simplebot.js
@@ -6,14 +6,14 @@ const client = new Discord.Client();
 const yesNoQuestion = /^l(izard)?[ -_]?bot,.+\?$/i;
 const whoQuestion   = /^l(izard)?[ -_]?bot, who[^?]+\?$/i; 
 const whySubExp     = /^l(izard)?[ -_]?bot, who/i;
-const ingRegExp     = /^[a-z]ing\.?$/i;
+const ingRegExp     = /^[A-Za-z]+ing$/;
 
 const eightBallReplies = ["I don't know.", "I don't care.", "Yes.", "Very yes!", "No.", "Very no!",
                           "Ask Leliel.", "I think so.", "I don't think so.", "Sure.",
                           "It will be soon.", "definitely!", "Oh my god, yes!", "🤔",
                           "If you believe in yourself it will be!", "You don't want to know the answer.",
                           "You're right!", "I think so.", "Maybe -_-", "I can't tell.", "Nah.",
-                          "Ew no/", "My connections say no.", "lol no!", "definitely not.",
+                          "Ew no", "lol no!", "Definitely not.", "Totally.",
                           "Literally yes!", "Literally no.", "Uhhhh...", "Pffft hahaha!",
                           "Let me google the answer for you...", "The answer is in your heart.",
                           "...", "Yes, only because I love you <3", "*nods*.", "100% sure!", "For sure!",
@@ -39,14 +39,14 @@ client.on("message", message =>
     // replacing _ing with _ong
     if (ingRegExp.test(message.content))
     {
-        message.channel.send(message.content[0] + "ong");
+        message.channel.send(message.content.slice(0,-3) + "ong");
     }
 
     // Answering "Lizard bot, who ___?"
     if (whoQuestion.test(message.content))
     {
         var target = pick(Array.from(message.channel.guild.members.values()));
-        var targetName = target.nickname !== null ? target.nickname : target.user.username;
+        var targetName = target.displayName;
 
         message.channel.send(message.content.replace(whySubExp, targetName).replace("?", "."));
 
